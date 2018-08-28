655a70593464ce9f04c9cd1fe988af39e1e69ff2
Aswin C <aswin@bititude.com>
2018-08-28 16:22:44 +0530

    inetial commit

--- 5.js
+++ 5.js
@@ -1,16 +1,16 @@
 // 5. Write a JavaScript program which accept a number as input and 
-// insert dashes (-) between each two even numbers. For example if you 
+// insert dashes (-) between each two even numbers. For eiample if you 
 // accept 025468 the output should be 0-254-6-8.
 
 var num = window.prompt();
 var str = num.toString();
 var result = [str[0]];
 
-for (var x = 1; x < str.length; x++) {
-  if (str[x - 1] % 2 === 0 && str[x] % 2 === 0) {
-    result.push('-', str[x]);
+for (var i = 1; i < str.length; i++) {
+  if (str[i - 1] % 2 === 0 && str[i] % 2 === 0) {
+    result.push('-', str[i]);
   } else {
-    result.push(str[x]);
+    result.push(str[i]);
   }
 }
 console.log(result.join(''));
--- 8.js
+++ 8.js
@@ -3,17 +3,17 @@
 // Fox' the output should be 'tHE qUICK bROWN fOX'.
 
 var str = 'The Quick Brown Fox';
-const UPPER = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-const LOWER = 'abcdefghijklmnopqrstuvwxyz';
+const up = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const low = 'abcdefghijklmnopqrstuvwxyz';
 const result = [];
   
 for(let x=0; x<str.length; x++)
 {
-if(UPPER.includes(str[x]))
+if(up.includes(str[x]))
 {
     result.push(str[x].toLowerCase());
 }
-else if(LOWER.includes(str[x]))
+else if(low.includes(str[x]))
 {
     result.push(str[x].toUpperCase());
 }
